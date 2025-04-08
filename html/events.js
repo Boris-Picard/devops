1ff1e72d533bbf300304000c62c73b28405a576c
Boris-Picard <picard.boris@gmail.com>
2025-04-08 14:25:33 +0200

    fix : correction js & json

--- html/events.js
+++ html/events.js
@@ -1,15 +1,15 @@
 fetch('events.json')
-  .then(res => res.json())
-  .then(data => {
-    const container = document.getElementById('liste');
-    data.forEach(event => {
-      const div = document.createElement('div');
-      div.innerHTML = `
+    .then(response => response.json())
+    .then(({ data }) => {
+        const container = document.getElementById('liste');
+        data.forEach(event => {
+            const div = document.createElement('div');
+            div.innerHTML = `
         <img src="${event.logo_url}" alt="" width="120" />
         <p>${event.location} - ${event.date}</p>
         <hr />
       `;
-      container.appendChild(div);
-    });
-  })
-  .catch(err => console.error('Erreur JSON :', err));
+            container.appendChild(div);
+        });
+    })
+    .catch(err => console.error('Erreur JSON :', err));
